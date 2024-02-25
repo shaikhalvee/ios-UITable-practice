29552260d35ec836e9fddc4fd738fcb9697731e7
Shaikh Islam <shaikhalvee@gmail.com>
2024-02-25 17:46:12 -0500

    Age part added

--- Assignment05/SelectAgeViewController.swift
+++ Assignment05/SelectAgeViewController.swift
@@ -8,16 +8,21 @@
 import UIKit
 
 class SelectAgeViewController: UIViewController {
-    
+    var ages = [Int]()
     var dataBackDelegate: DataBackDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Populate the ages array from 18 to 64
+        for age in 18...100 {
+            ages.append(age)
+        }
     }
     
-
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -28,4 +33,26 @@ class SelectAgeViewController: UIViewController {
     }
     */
 
+}
+
+extension SelectAgeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ageViewCell", for: indexPath)
+        let age = ages[indexPath.row]
+        cell.textLabel?.text = String(age)
+        return cell
+    }
+}
+
+extension SelectAgeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let choice = ages[indexPath.row]
+        let user = User(age: choice)
+        self.dataBackDelegate?.sendBackAgeData(user: user)
+        dismiss(animated: true)
+    }
 }
