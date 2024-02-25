b174b9beb22152f40b8601925a7efa05909166c4
Shaikh Islam <shaikhalvee@gmail.com>
2024-02-25 11:42:11 -0500

    user view updated

--- Assignment05/UserDetailsViewController.swift
+++ Assignment05/UserDetailsViewController.swift
@@ -8,14 +8,42 @@
 import UIKit
 
 class UserDetailsViewController: UIViewController {
+    
+    var userDetails: User?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.nameTextLabel.text = userDetails?.name
+        self.emailTextLabel.text = userDetails?.email
+        self.ageTextLabel.text = "\(userDetails!.age) years old"
+        self.stateTextLabel.text = userDetails?.state
+        self.genderTextLabel.text = userDetails?.gender
+        self.groupTextLabel.text = userDetails?.group
     }
     
-
+    @IBOutlet weak var nameTextLabel: UILabel!
+    @IBOutlet weak var emailTextLabel: UILabel!
+    @IBOutlet weak var ageTextLabel: UILabel!
+    @IBOutlet weak var stateTextLabel: UILabel!
+    @IBOutlet weak var genderTextLabel: UILabel!
+    @IBOutlet weak var groupTextLabel: UILabel!
+   
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        let lables = [nameTextLabel, emailTextLabel, ageTextLabel, stateTextLabel, genderTextLabel, groupTextLabel]
+         lables.forEach{$0?.text = ""}
+         
+         
+         let alertController = UIAlertController(title: "Data Deleted", message: "Click the back button to go back.", preferredStyle: .alert)
+             alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+             present(alertController, animated: true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
